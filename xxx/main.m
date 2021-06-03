5efe664f2ed811dd6034220c852d69f308c3668a
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-06-03 23:21:36 +0200

    Initial Commit

---
+++ xxx/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xxx
+//
+//  Created by Dirk-Willem van Gulik on 03/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
